a72392c3515f7b214234ea11159963ff2b40ee6c
Olga <olga1111vishneva.v@mail.ru>
2025-06-02 15:32:20 +0300

    Найти min/max в строке

---
+++ 3-module/5-task/index.js
@@ -0,0 +1,13 @@
+function getMinMax(str) {
+  const numbers = str
+      .split(' ')
+      .map(Number)
+      .filter(num => !isNaN(num));
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+
+  return {
+    min: min,
+    max: max
+  };
+}
