3b5d671b2ca25c9bec37fcd0aca428827699cae4
Olga <olga1111vishneva.v@mail.ru>
2025-06-04 16:37:28 +0300

    Выделите ячейки по диагонали

---
+++ 4-module/2-task/index.js
@@ -0,0 +1,10 @@
+function makeDiagonalRed(table) {
+  const rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i] && rows[i].cells[i]) {
+      const cell = rows[i].cells[i];
+      cell.style.backgroundColor = 'red';
+    }
+  }
+}
